96b0acdc19cacfd725b595d7e0841b111300e4d9
shiv <s@gmail.com>
2025-07-02 16:05:15 +0530

    change in config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+// next.config.mjs
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
 
 export default nextConfig;
